da15cad4f353f4591bbe52c57db4d4385bb4d083
EasyKidsRobotics <66917385+EasyKidsRobotics@users.noreply.github.com>
2023-08-01 12:57:48 +0700

    Update EasyKids3in1Robot.h

--- EasyKids3in1Robot.h
+++ EasyKids3in1Robot.h
@@ -32,29 +32,6 @@ void EasyKids_Setup(){
     pwm.setPWM(5, 0, 0);
     pwm.setPWM(6, 0, 0);
     pwm.setPWM(7, 0, 0);
-}
-void EasyKidsJoystick_Setup(){
-    Serial.begin(115200);
-    display.begin();
-    displayClear();
-    rgb.begin();
-    rgbCar.begin();
-    ledSetBrightness(20);
-    ledCarSetBrightness(20);
-    ledFillColor(BLACK);
-    ledCarFillColor(BLACK);
-    pwm.begin();
-    pinMode(sw, INPUT);
-    pinMode(BUZZER, OUTPUT);
-    pwm.setPWMFreq(50);
-    pwm.setPWM(0, 0, 0);
-    pwm.setPWM(1, 0, 0);
-    pwm.setPWM(2, 0, 0);
-    pwm.setPWM(3, 0, 0);
-    pwm.setPWM(4, 0, 0);
-    pwm.setPWM(5, 0, 0);
-    pwm.setPWM(6, 0, 0);
-    pwm.setPWM(7, 0, 0);
     Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
     const uint8_t *addr = BP32.localBdAddress();
     Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2],
